52c04ef0e9d3949243b369e927ef7bce77257499
Hervesson <hervessonporto@hotmail.com>
2025-09-15 09:44:58 -0300

    refactor: passing token in route

--- src/helpers/healthUnits.js
+++ src/helpers/healthUnits.js
@@ -1,8 +1,41 @@
 'use server'
 
-// import { TOKEN_KEY } from '../../../middleware'
+import { cookies } from 'next/headers'
+import { TOKEN_KEY } from '../app/middleware'
 
 import api from './api'
+
+export async function CreateUnit(payload) {
+  try {
+    const cookie = await cookies()
+    const token = cookie.get(TOKEN_KEY)
+
+    const response = await api.post('/unidades-saude', payload, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token.value,
+      },
+    })
+
+    return {
+      success: true,
+      data: response.data,
+    }
+  } catch (error) {
+    const fallback = {
+      message: 'Erro desconhecido ao tentar criar unidade',
+      statusCode: 500,
+      error: 'UnknownError',
+    }
+
+    const errData = error?.response?.data || fallback
+
+    return {
+      success: false,
+      error: errData,
+    }
+  }
+}
 
 export async function listAllUnits() {
   try {
